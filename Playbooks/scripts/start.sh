105e61c4efacec6ec592061c98e813bebb685d9e
Andreas Brandting <Andreas@brandting.com>
2024-12-12 00:15:54 +0100

    Add company banner to start script

--- Playbooks/scripts/start.sh
+++ Playbooks/scripts/start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 echo "Running Tech Solutions AB kubernetes setup"
-ansible-playbook -i ../inventory/inventory.ini ../setup_kubernetes.yaml --ask-vault-pass
+ansible-playbook -i inventory/inventory.ini setup_kubernetes.yaml --ask-vault-pass
 
