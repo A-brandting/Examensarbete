d6cdeabcc34bc4d97a77bc6dacc6431ed8b47068
Andreas Brandting <Andreas@brandting.com>
2024-12-13 10:15:48 +0100

    Add portforwarding to update.sh

--- Playbooks/scripts/update.sh
+++ Playbooks/scripts/update.sh
@@ -1,1 +1,6 @@
-ansible-playbook -i inventory/inventory.ini update_servers.yaml --ask-vault-pass
+# Runs update-playbook
+ansible-playbook -i ../inventory/inventory.ini ../update_servers.yaml --ask-vault-pass
+
+# Portforward localhost:8080
+screen -dmS my-port-forward bash -c "kubectl port-forward svc/ts-webserver-01 8080:80 -n ts-development"
+
