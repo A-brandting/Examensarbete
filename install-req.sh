d2172b2c1225c99f19651ac166d4eb325f3f0671
Andreas Brandting <Andreas@brandting.com>
2024-12-16 15:24:36 +0100

    Add python3 to install-req

--- install-req.sh
+++ install-req.sh
@@ -3,8 +3,15 @@
 # Uppgraderar systemet
 sudo apt update && apt upgrade -y
 
-# Klonar git-repo till mappen som skriptet körs ifrån
+# Installerar python-beroende för ansible-galaxy
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+
+# Installerar git
 sudo apt install git -y
+
+# Klonar git-repo till mappen som skriptet körs ifrån
+git clone https://github.com/A-brandting/Examensarbete.git
 
 # Installerar ansible
 sudo apt install ansible -y
@@ -13,6 +20,6 @@ sudo apt install ansible -y
 sudo ansible-galaxy collection install kubernetes.core
 
 # Uppgraderar ansible
-pip install --upgrade ansible
+sudo pip install --upgrade ansible
 
 
