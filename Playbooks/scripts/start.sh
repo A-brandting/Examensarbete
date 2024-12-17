99481b5ec2e663d18534a849bb1e25b35a99dbae
Andreas Brandting <Andreas@brandting.com>
2024-12-18 00:32:16 +0100

    Rename files

--- Playbooks/scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "Running Tech Solutions AB kubernetes setup"
-
-# Runs setup_kubernetes playbook
-ansible-playbook -i ../inventory/inventory.ini ../setup_kubernetes.yaml --ask-vault-pass
-
