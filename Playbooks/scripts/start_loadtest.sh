c07895245abc886051b7f9d7545dab56e3ca3c11
Andreas Brandting <Andreas@brandting.com>
2024-12-18 10:05:50 +0100

    Fix start loadtest script

--- Playbooks/scripts/start_loadtest.sh
+++ Playbooks/scripts/start_loadtest.sh
@@ -1,2 +1,2 @@
 echo "Running Tech Solutions AB Load Test"
-ansible-playbook -i inventory/inventory.ini loadtest.yaml --ask-vault-pass
+ansible-playbook -i ../inventory/inventory.ini ../loadtest.yaml --ask-vault-pass
