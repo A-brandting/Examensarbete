dc86ab5eb3286f3c9692e71abb820eb1343947f2
Andreas Brandting <Andreas@brandting.com>
2024-12-18 10:02:42 +0100

    Change directory structure

--- Playbooks/k8s-environments/development/scripts/check-status.sh
+++
@@ -1,6 +0,0 @@
-echo ""
-echo -e "\e[32m[ *** Pods: ts-development namespace *** ]\e[0m"
-kubectl get pods -n ts-development -o wide
-echo ""
-echo -e "\e[32m[ *** Horizonal Pod Scaling info *** ]\e[0m"
-kubectl get hpa -n ts-development
--- Playbooks/k8s-environments/development/scripts/load-test-webserver-01.sh
+++
@@ -1,2 +0,0 @@
-sudo apt-get install apache2-utils
-ab -n 30000 -c 1000 http://localhost:8080/
